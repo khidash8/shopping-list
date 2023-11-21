f036dda72c8f06f3e12de9d8210711e0f2aff6d8
Anxious Ape <iamkhidash@gmail.com>
2023-11-21 12:34:33 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,51 @@
+//! Get all the elements
+const itemForm = document.querySelector("#item-form");
+const itemInput = document.querySelector("#item-input");
+const itemList = document.querySelector("#item-list");
+
+//! Functions
+function addListItems(e) {
+  e.preventDefault();
+
+  const newData = itemInput.value;
+
+  //? create list element
+  const li = document.createElement("li");
+  li.append(document.createTextNode(newData));
+
+  //? inside li, we need button element with fontawsom icon
+  const button = createButton("remove-item btn-link text-red");
+
+  //? append button to li
+  li.appendChild(button);
+
+  //? append li to parent list(ul)
+  itemList.appendChild(li);
+
+  //? clear input field after submitting
+  itemInput.value = "";
+}
+
+//? Create Button and icons
+function createButton(classes) {
+  //* button
+  const button = document.createElement("button");
+  button.className = classes;
+
+  //* icon
+  const icon = createIcon("fa-solid fa-xmark");
+
+  //* append icon to button
+  button.appendChild(icon);
+
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
+}
+
+//! Events
+itemForm.addEventListener("submit", addListItems);
